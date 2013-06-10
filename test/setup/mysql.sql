003347c484e957ea83be4e046feaf394b55cfb16
Andrew Judd <awjudd@gmail.com>
2013-06-10 06:53:21 -0400

    Adding in a script to generate the test database

---
+++ test/setup/mysql.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `foo`
+(
+  `foo` varchar(10) NOT NULL,
+  `bar` int(11) NOT NULL,
+  `foobar` float NOT NULL,
+  `blah` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `foo` (`foo`, `bar`, `foobar`, `blah`) VALUES
+('asdf', 0, 10, 'asdf'),
+('Andrew', 0, 10.3, 'Test 1'),
+('Testing', 12, 12.5, 'Test 2');
