5365d640beab0c82fe59a1d47772c7e2df2d75da
Andrew Judd <contact@andrewjudd.ca>
2017-01-09 23:42:42 -0500

    Adding in documentation

--- src/Database/ConnectionString.php
+++ src/Database/ConnectionString.php
@@ -7,16 +7,29 @@ use Awjudd\PDO\Database\Configuration;
 class ConnectionString
 {
 
+    /**
+     * Derives the connection string based on the given configuration.
+     *
+     * @param      \Awjudd\PDO\Database\Configuration  $configuration  The configuration
+     *
+     * @return     string                              The connection string 
+     */
     public static function derive(Configuration $configuration)
     {
         switch($configuration->engine)
         {
             case 'mysql':
                 return static::mysql($configuration);
-                break;
         }
     }
 
+    /**
+     * Derives the connection string for a Mysql connection
+     *
+     * @param      \Awjudd\PDO\Database\Configuration  $configuration  The configuration
+     *
+     * @return     string                               The mysql specific connection string
+     */
     public static function mysql(Configuration $configuration)
     {
         return sprintf(
